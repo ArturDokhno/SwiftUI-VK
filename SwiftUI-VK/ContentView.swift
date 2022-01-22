d1c692ff9cd62061b689b63689a1772e7e54d8c2
ArturDokhno <artur.dohno@icloud.com>
2022-01-22 19:51:49 +0500

    Initial Commit

---
+++ SwiftUI-VK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-VK
+//
+//  Created by Артур Дохно on 22.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-VK/SwiftUI_VKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VKApp.swift
+//  SwiftUI-VK
+//
+//  Created by Артур Дохно on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
