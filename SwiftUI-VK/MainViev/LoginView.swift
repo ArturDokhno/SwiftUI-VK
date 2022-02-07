aed60e78351d20221349ddf8acb660ab50ec9d62
ArturDokhno <artur.dohno@icloud.com>
2022-02-07 19:59:22 +0500

    Правки LoginView. Скрытие приветствия и логотипа для того что бы клавиатура не перекрывала кнопку "Log In". Рефакторинг кода а именно LoginView.

--- SwiftUI-VK/MainViev/LoginView.swift
+++ SwiftUI-VK/MainViev/LoginView.swift
@@ -6,56 +6,56 @@
 //
 
 import SwiftUI
+import Combine
 
 let lightGreyColor = Color(red: 239.0/255.0,
                            green: 243.0/255.0,
                            blue: 244.0/255.0)
 
 struct LoginView: View {
-    
     @State var username: String = ""
     @State var password: String = ""
-    
+    @State var showAlert: Bool = false
+    @State private var shouldShowLogo: Bool = true
     @Binding var showMainScreen: Bool
     
-    @State var authenticationDidfail: Bool = false
-    @State var authenticationDidSucceed: Bool = false
+    
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false }
+    )
+        .removeDuplicates()
     
     var body: some View {
-        ZStack {
+        ScrollView {
             VStack {
-                HelloText()
-                VKImage()
-                UsernameTextField(username: $username)
-                PasswordSecureField(password: $password)
-                
-                if authenticationDidfail {
-                    Text("Information not correct. Try again.")
-                        .offset(y: -10)
-                        .foregroundColor(.red)
+                if shouldShowLogo {
+                    HelloText()
+                    VKImage()
                 }
-                
-                Button {
-                    if self.username == "Artur" && self.password == "1234" {
-                        self.authenticationDidSucceed = true
-                        self.authenticationDidfail = false
-                        self.showMainScreen = true
-                    } else {
-                        self.authenticationDidfail = true
-                        self.authenticationDidSucceed = false
-                    }
-                } label: {
-                    LoginButton()
-                }
+                TextFiedls(isSecure: false, text: $username)
+                TextFiedls(isSecure: true, text: $password)
+                LoginButton(username: $username,
+                            password: $password,
+                            showAlert: $showAlert,
+                            showMainScreen: $showMainScreen)
             }
-            
             .padding()
-            if authenticationDidSucceed {
-                Text("Login succeded!")
-                    .font(.headline)
-                    .foregroundColor(.green)
-                    .animation(Animation.default)
+        }
+        .onReceive(keyboardIsOnPublisher) { isKeyboardOn in
+            withAnimation(Animation.easeInOut(duration: 0.5)) {
+                self.shouldShowLogo = !isKeyboardOn
             }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Error"),
+                  message: Text("Incorect data"),
+                  dismissButton: .cancel())
+        }
+        .onTapGesture {
+            UIApplication.shared.endEditing()
         }
     }
 }
@@ -79,40 +79,52 @@ struct VKImage: View {
 }
 
 struct LoginButton: View {
-    var body: some View {
-        Text("Log In")
-            .font(.headline)
-            .foregroundColor(.white)
-            .padding()
-            .frame(width: 220, height: 60)
-            .background(.black)
-            .cornerRadius(35)
-    }
-}
-
-struct UsernameTextField: View {
-    
     @Binding var username: String
+    @Binding var password: String
+    @Binding var showAlert: Bool
+    @Binding var showMainScreen: Bool
     
     var body: some View {
-        TextField("Username", text: $username)
-            .padding()
-            .background(lightGreyColor)
-            .cornerRadius(10.0)
-            .padding(.bottom, 20)
+        Button {
+            if username == "Artur" && password == "1234" {
+                showMainScreen = true
+            } else {
+                showAlert = true
+            }
+        } label: {
+            Text("Log In")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(.black)
+                .cornerRadius(35)
+        }
     }
 }
 
-struct PasswordSecureField: View {
-    
-    @Binding var password: String
+struct TextFiedls: View {
+    let isSecure: Bool
+    @Binding var text: String
     
     var body: some View {
-        SecureField("Password", text: $password)
-            .padding()
-            .background(lightGreyColor)
-            .cornerRadius(10.0)
-            .padding(.bottom, 20)
+        Group {
+            if isSecure {
+                SecureField("Password", text: $text)
+            } else {
+                TextField("Username", text: $text)
+            }
+        }
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(10.0)
+        .padding(.bottom, 20)
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
